b4cb033ffd1968a2942f979b9537365d6d511bc3
AndruhaPower <pazit@yandex.ru>
2020-01-19 15:36:17 +0300

    initial commit

---
+++ VideoEditorTape/VideoEditorTape/VideoTapeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VideoTapeViewController.swift
+//  VideoEditorTape
+//
+//  Created by Andrew on 19/01/2020.
+//  Copyright © 2020 Andrew. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoTapeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .cyan
+    }
+}
+
