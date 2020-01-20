4a957c10864135b3a5453caa9b79b96c4cd9e21d
AndruhaPower <pazit@yandex.ru>
2020-01-21 00:54:13 +0300

    Кастомная ячейка для CollectionView

---
+++ VideoEditorTape/VideoEditorTape/TapeCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  TapeCollectionViewCell.swift
+//  VideoEditorTape
+//
+//  Created by Andrew on 20/01/2020.
+//  Copyright © 2020 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class TapeCollectionViewCell: UICollectionViewCell {
+    
+    static let reuseId: String = "thumbnailCell"
+    
+    var imageView: UIImageView =  {
+       let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupView()
+        self.setNeedsLayout()
+        self.layoutIfNeeded()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        self.contentView.addSubview(self.imageView)
+        
+        self.imageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
+        self.imageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
+        self.imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
+        self.imageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
+    }
+}
