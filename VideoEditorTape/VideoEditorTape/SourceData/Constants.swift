6d7b9a2f603ac73016a28f059a52db31fbdea255
AndruhaPower <pazit@yandex.ru>
2020-01-21 23:43:46 +0300

    константы + зашитый метод для определения размера картинки, при учете того что на экране всего 6 картинок показывается

--- VideoEditorTape/VideoEditorTape/SourceData/Constants.swift
+++ VideoEditorTape/VideoEditorTape/SourceData/Constants.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Andrew. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 
 class Constants {
@@ -14,5 +14,19 @@ class Constants {
     static let videoPath: String = {
         guard let path = Bundle.main.path(forResource: "cute_puppies", ofType: "mp4") else { return ""}
         return path
+    }()
+    
+    static let imageSize: CGSize = {
+        let imagesPerScreen: CGFloat = 6
+        let width = UIScreen.main.bounds.size.width
+        let height = UIScreen.main.bounds.size.height
+        guard height != 0 else { return CGSize.zero }
+        let ratio = width / height
+        let imageWidth = width / imagesPerScreen
+        guard ratio != 0 else { return CGSize.zero }
+        let imageHeight = imageWidth / ratio
+        
+        let frame = CGSize(width: imageWidth, height: imageHeight)
+        return frame
     }()
 }
